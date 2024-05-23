9181e33d50055867624e73b046eabe12d10f9e0a
Matin Mir <asmamir297@gmail.com>
2024-05-23 14:14:38 +0330

    implement a server side compatible select component

---
+++ components/ui/select.tsx
@@ -0,0 +1,19 @@
+import { HTMLProps, forwardRef } from "react";
+import { cn } from "@/lib/utils";
+
+const Select = forwardRef<HTMLSelectElement, HTMLProps<HTMLSelectElement>>(
+  function ({ className, ...props }, ref) {
+    return (
+      <select
+        ref={ref}
+        className={cn(
+          "flex h-10 w-full truncate rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        {...props}
+      />
+    );
+  },
+);
+
+export { Select };
