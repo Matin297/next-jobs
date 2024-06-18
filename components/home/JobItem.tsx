761bf71a7c3fbab61a0ce71a9366df435e2fb90e
Matin Mir <asmamir297@gmail.com>
2024-06-18 19:16:26 +0330

    wrap job item in a link to its details page

--- components/home/JobItem.tsx
+++ components/home/JobItem.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import { CustomJobType } from "@/lib/data";
 import { formatSalary, calcDateDistFromNow } from "@/lib/utils";
@@ -6,6 +7,7 @@ import CompanyLogoPlaceholder from "@/assets/company-logo-placeholder.png";
 
 export default function JobItem({
   type,
+  slug,
   title,
   style,
   salary,
@@ -15,54 +17,56 @@ export default function JobItem({
 }: CustomJobType) {
   return (
     <li>
-      <article className="flex items-start gap-5 rounded border p-3 hover:bg-muted/30">
-        <Image
-          width={100}
-          height={100}
-          alt="company name"
-          src={company.logoURL || CompanyLogoPlaceholder}
-        />
-        <div>
-          <div className="mb-5">
-            <h2 className="text-lg font-bold">{title}</h2>
-            <h3 className="text-xs text-muted-foreground">{company.name}</h3>
+      <Link href={`/job/${slug}`}>
+        <article className="flex items-start gap-5 rounded border p-3 hover:bg-muted/30">
+          <Image
+            width={100}
+            height={100}
+            alt="company name"
+            src={company.logoURL || CompanyLogoPlaceholder}
+          />
+          <div>
+            <div className="mb-5">
+              <h2 className="text-lg font-bold">{title}</h2>
+              <h3 className="text-xs text-muted-foreground">{company.name}</h3>
+            </div>
+
+            <div className="space-y-3 text-sm text-muted-foreground">
+              <div className="flex items-center gap-2 sm:hidden">
+                <Briefcase size={16} />
+                <span>{type}</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <MapPin size={16} />
+                <span>{style}</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <Globe2 size={16} />
+                <span>{`${location.city}, ${location.country}`}</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <Banknote size={16} />
+                <span>{salary ? formatSalary(salary) : "----"}</span>
+              </div>
+              <div className="flex items-center gap-2 sm:hidden">
+                <Clock size={16} />
+                <span>{calcDateDistFromNow(updatedAt)}</span>
+              </div>
+            </div>
           </div>
 
-          <div className="space-y-3 text-sm text-muted-foreground">
-            <div className="flex items-center gap-2 sm:hidden">
+          <div className="ml-auto hidden flex-col justify-between self-stretch text-sm sm:flex">
+            <div className="flex items-center gap-2 rounded border bg-muted p-2 text-muted-foreground">
               <Briefcase size={16} />
               <span>{type}</span>
             </div>
-            <div className="flex items-center gap-2">
-              <MapPin size={16} />
-              <span>{style}</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <Globe2 size={16} />
-              <span>{`${location.city}, ${location.country}`}</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <Banknote size={16} />
-              <span>{salary ? formatSalary(salary) : "----"}</span>
-            </div>
-            <div className="flex items-center gap-2 sm:hidden">
+            <div className="flex items-center gap-2 text-muted-foreground">
               <Clock size={16} />
               <span>{calcDateDistFromNow(updatedAt)}</span>
             </div>
           </div>
-        </div>
-
-        <div className="ml-auto hidden flex-col justify-between self-stretch text-sm sm:flex">
-          <div className="flex items-center gap-2 rounded border bg-muted p-2 text-muted-foreground">
-            <Briefcase size={16} />
-            <span>{type}</span>
-          </div>
-          <div className="flex items-center gap-2 text-muted-foreground">
-            <Clock size={16} />
-            <span>{calcDateDistFromNow(updatedAt)}</span>
-          </div>
-        </div>
-      </article>
+        </article>
+      </Link>
     </li>
   );
 }
